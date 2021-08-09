f7834055bd808fc6857c1715e2430bf81b553e8d
AhmedThabet569 <thabeta532@gmail.com>
2021-08-09 11:21:48 +0200

    first commit

---
+++ assets/js/map-markers-samples/sampleMapContactMarker.js
@@ -0,0 +1,9 @@
+var mapMarkers = [
+    {
+        title: "موقعنا الرئيسي",
+        address: "20 شارع الملك عبد العزيز الرياض",
+        lat: 24.774265,
+        lng:  46.738586,
+        img: "assets/images/contact/1.jpg"
+    }
+];
